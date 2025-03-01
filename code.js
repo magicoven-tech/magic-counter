5cf311ba07248d2423c62341793a1fc49086a447
brunonepomuceno <brunobezerranepomuceno@gmail.com>
2025-03-01 12:54:35 -0300

    amazing UI

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-figma.showUI(__html__, { width: 300, height: 600 });
+figma.showUI(__html__, { width: 580, height: 600 });
 
 figma.ui.onmessage = (message) => {
   if (message.type === 'count-frames') {
